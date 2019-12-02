752e97a2a8d24594ec2b445f28b29289bd02984b
Mark Hofmann <mark@mark-hofmann.de>
2019-12-02 12:34:54 +0700

    added comment

--- core/src/main/kotlin/DatabaseSettings.kt
+++ core/src/main/kotlin/DatabaseSettings.kt
@@ -7,6 +7,8 @@ open class DatabaseSettings(
     var host: String = "localhost",
     var database: String = "postgres",
     var port: Int = 5432,
+    /** Please note that the schema is already hardcoded in the generated jOOQ classes. This property is used to pass
+     * the schema name to custom code, like a liquibase updater or in cases where you have common database classes. */
     var schema: String = "public",
     var maxPoolSize: Int = 10,
     var username: String = "postgres",
