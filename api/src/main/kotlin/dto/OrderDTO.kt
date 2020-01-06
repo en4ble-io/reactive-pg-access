0980b9bb5f929597be7091e52c7285930d794ef0
Mark Hofmann <mark@mark-hofmann.de>
2020-01-06 10:52:05 +0700

    fixes OrderDTO default constructor

--- api/src/main/kotlin/dto/OrderDTO.kt
+++ api/src/main/kotlin/dto/OrderDTO.kt
@@ -7,5 +7,5 @@ import io.en4ble.pgaccess.enumerations.SortDirection
  * @author Mark Hofmann (mark@en4ble.io)
  */
 @Suppress("unused")
-class OrderDTO constructor(val field: String, val direction: SortDirection)
+class OrderDTO constructor(val field: String = "created", val direction: SortDirection = SortDirection.DESC)
 
